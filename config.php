065fd15f5263368dc44944be7ea67043492a9218
Soluna - IT <ruben@solunapilates.es>
2016-02-10 12:21:05 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Change between test and live environment
+define('entorno_tpv','live');
+define('clave_tpv','TPV_KEY_SHA256');  
+
+//define('entorno_tpv','test');
+//define('clave_tpv','TPV_KEY_SHA256_TEST_ENVIRONMENT');    
+
+?>
